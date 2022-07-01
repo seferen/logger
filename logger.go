d6fe7753ac0ccfba5e8ef2665f1151a3a0df54cb
Ivan <79854463009@yandex.ru>
2022-07-02 00:06:53 +0300

    change logic of logger (#2)
    * change logic of logger, add individual logger for each instanse
    
    * add some tests
    
    Co-authored-by: ZUBOV Ivan S <ivan.zubov@raiffeisen.ru>

--- logger.go
+++ logger.go
@@ -7,19 +7,21 @@ import (
 )
 
 const (
-	DebugLogLevel int = 0
-	InfoLogLevel  int = 1
-	WarnLogLevel  int = 2
-	ErrorLogLevel int = 3
+	DebugLogLevel int = iota
+	InfoLogLevel  int = iota
+	WarnLogLevel  int = iota
+	ErrorLogLevel int = iota
+	OffLogLevel   int = iota
 	otput             = log.Ldate | log.Ltime | log.Lmsgprefix
 )
 
 var (
-	logers = make(map[string]*logger)
+	rootLevel *int = new(int)
+	loggers        = make(map[string]*logger)
 )
 
 type logger struct {
-	logLevel    int
+	logLevel    *int
 	infoLogger  *log.Logger
 	debubLogger *log.Logger
 	errorLogger *log.Logger
@@ -28,43 +30,53 @@ type logger struct {
 
 func NewLogger(name string) *logger {
 	l := logger{}
-	l.logLevel = 1
+	l.logLevel = rootLevel
 	l.infoLogger = log.New(os.Stdout, fmt.Sprint("INFO ", name, "\t"), otput)
 	l.debubLogger = log.New(os.Stdout, fmt.Sprint("DEBUG ", name, "\t"), otput)
 	l.errorLogger = log.New(os.Stdout, fmt.Sprint("ERROR ", name, "\t"), otput)
 	l.warnLogger = log.New(os.Stdout, fmt.Sprint("WARN ", name, "\t"), otput)
 
-	logers[name] = &l
+	loggers[name] = &l
 	return &l
 }
 
 func (l *logger) Debug(v ...interface{}) {
-	if l.logLevel <= DebugLogLevel {
+	if *l.logLevel >= DebugLogLevel {
 		l.debubLogger.Println(v...)
 	}
 }
 
 func (l *logger) Info(v ...interface{}) {
-	if l.logLevel <= InfoLogLevel {
+	if *l.logLevel >= InfoLogLevel {
 		l.infoLogger.Println(v...)
 	}
 }
 func (l *logger) Warn(v ...interface{}) {
-	if l.logLevel <= WarnLogLevel {
+	if *l.logLevel >= WarnLogLevel {
 		l.warnLogger.Println(v...)
 	}
 }
 func (l *logger) Error(v ...interface{}) {
-	if l.logLevel <= ErrorLogLevel {
-		l.errorLogger.Panicln(v...)
-	}
+	l.errorLogger.Println(v...)
+
+}
+func (l *logger) SetLogLevel(logLevel int) {
+	l.logLevel = &logLevel
 }
 
 func GetLoger(name string) (logger *logger) {
-	if v, ok := logers[name]; ok {
+	if v, ok := loggers[name]; ok {
 		return v
 	} else {
 		logger = NewLogger(name)
 	}
 	return
+}
+
+func SetRootLevel(logLevel int) {
+	*rootLevel = logLevel
+}
+
+func init() {
+	*rootLevel = 1
 }
--- logger_test.go
+++ logger_test.go
@@ -1,1 +1,70 @@
 package logger
+
+import "testing"
+
+func TestNewLogger(t *testing.T) {
+
+	logerName := "test"
+
+	l := NewLogger(logerName)
+
+	if *l.logLevel != 1 {
+		t.FailNow()
+	}
+
+	if v, ok := loggers[logerName]; !ok || v != l {
+		t.FailNow()
+	}
+}
+
+func TestSetRootLevel(t *testing.T) {
+	logerName := "test"
+
+	loglevel := 2
+
+	SetRootLevel(loglevel)
+
+	if *rootLevel != loglevel {
+		t.FailNow()
+	}
+
+	l := NewLogger(logerName)
+
+	if l.logLevel != rootLevel || *l.logLevel != loglevel {
+		t.FailNow()
+	}
+
+}
+
+func TestGetLogger(t *testing.T) {
+	logerName := "test"
+	logerName2 := "test2"
+
+	l := GetLoger(logerName)
+
+	if v, ok := loggers[logerName]; !ok || v != l {
+		t.FailNow()
+	}
+
+	l = GetLoger(logerName2)
+
+	if v, ok := loggers[logerName2]; !ok || v != l {
+		t.FailNow()
+	}
+
+}
+
+func TestSetLogLevel(t *testing.T) {
+	logerName3 := "test3"
+	logLevel := 3
+	l := NewLogger(logerName3)
+
+	if l.logLevel != rootLevel {
+		t.Failed()
+	}
+
+	if l.SetLogLevel(logLevel); logLevel == *rootLevel {
+		t.Failed()
+	}
+
+}
